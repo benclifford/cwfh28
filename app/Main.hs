a4c190c180f0e7488bad51422bf99c396fccd3cb
Ben Clifford <benc@hawaga.org.uk>
2018-02-06 12:11:10 +0000

    Post 5

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,16 @@ import qualified Database.PostgreSQL.Simple as PG
 import Control.Exception (bracket)
 import Control.Monad.IO.Class (liftIO)
 
+import Data.Monoid ( (<>) )
+
 import Servant.HTML.Blaze as SB
 import qualified Text.Blaze.Html5 as B
+import Text.Blaze.Html5 ( (!) )
+import qualified Text.Blaze.Html5.Attributes as BA
+
+import qualified Text.Digestive as DF
+import Text.Digestive ( (.:) )
+import Text.Digestive.Blaze.Html5 as DB
 
 import Servant ( (:>), ( :<|> )(..) )
 import qualified Servant as S
@@ -25,9 +33,13 @@ type HtmlPingAPI = "htmlping" :> S.Get '[SB.HTML] B.Html
 
 type RegistrationAPI = "registration" :> S.Capture "id" Integer :> S.Get '[SB.HTML] B.Html
 
+type RegistrationPostAPI = "registration" :> S.Capture "id" Integer :> S.ReqBody '[S.FormUrlEncoded] [(String,String)] :> S.Post '[HTML] B.Html
+
+
 type API = PingAPI
       :<|> HtmlPingAPI
       :<|> RegistrationAPI
+      :<|> RegistrationPostAPI
 
 handlePing :: S.Handler String
 handlePing = return "PONG"
@@ -51,16 +63,39 @@ handleRegistration identifier = do
         "SELECT firstname, lastname, dob FROM registration WHERE id = ?"
         [identifier] :: IO [Registration]
 
+  view <- DF.getForm "Registration" (registrationDigestiveForm registration)
+
   return $ B.docTypeHtml $ do
     B.body $ do
       B.h1 $ do "Registration "
                 B.toHtml (show identifier)
+      htmlForRegistration view
+
+handleRegistrationPost = error "We'll implement this later"
+
+htmlForRegistration :: DF.View B.Html -> B.Html
+htmlForRegistration view =
+  B.form
+    ! BA.method "post"
+    $ do
       B.p $ do  "First name: "
-                B.toHtml (firstname registration)
+                DB.inputText "firstname" view
       B.p $ do  "Last name: "
-                B.toHtml (lastname registration)
+                DB.inputText "lastname" view
       B.p $ do  "Date of Birth: "
-                B.toHtml (dob registration)
+                DB.inputText "dob" view
+      B.p $     DB.inputSubmit "Save" 
+
+
+
+registrationDigestiveForm :: Monad m => Registration -> DF.Form B.Html m Registration
+registrationDigestiveForm initial = do
+  Registration
+    <$> "firstname" .: DF.string (Just $ firstname initial)
+    <*> "lastname" .: DF.string (Just $ lastname initial)
+    <*> "dob" .: DF.string (Just $ dob initial)
+
+
 
 api :: S.Proxy API
 api = S.Proxy
@@ -69,4 +104,6 @@ app = S.serve api server
 
 server :: S.Server API
 server = handlePing :<|> handleHtmlPing :<|> handleRegistration
+    :<|> handleRegistrationPost
+
 
